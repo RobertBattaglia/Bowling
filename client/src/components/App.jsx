1ac149ad3b47f4b6b41a7501aae040a779e6c8da
Andrew <Andrewkn09@Gmail.com>
2019-07-02 19:36:49 -0400

    Complete handleClick

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,9 +8,78 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pins: Array(10).fill(1)
+      pins: Array(10).fill(1),
+      shot: 1,
+      frames: [],
+      frame: {
+        shot1: 0,
+        shot2: 0,
+        score: 0
+      },
+      currFrame: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
+
+  handleClick(num) {
+    let { frames, frame, currFrame, shot } = this.state;
+    if (num === 10 && shot === 1) {
+      let newFrames = frames.slice();
+      newFrames.push({ shot1: 10, shot2: '-', score: 10 });
+      this.setState({
+        pins: Array(10).fill(0),
+        shot: 2,
+        frames: newFrames
+      });
+    } else {
+      let pins = this.state.pins.slice();
+      let index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      for (let i = 0; i < 10 - num; i++) {
+        var random = Math.floor(Math.random() * Math.floor(index.length));
+        index.splice(random, 1);
+      }
+      let counter = num;
+      while (counter > 0) {
+        var random = Math.floor(Math.random() * Math.floor(index.length));
+        let temp = index.splice(random, 1);
+        pins[temp] = 0;
+        counter--;
+      }
+
+      let score = pins.filter(pin => {
+        return pin === 0;
+      }).length;
+
+      let newFrame = Object.assign({}, frame);
+      if (shot === 1) {
+        newFrame.shot1 = num;
+        newFrame.score = num;
+      } else {
+        newFrame.shot2 = score;
+        newFrame.score = num + score;
+      }
+
+      let newFrames = frames.slice();
+      newFrames[currFrame] = newFrame;
+      this.setState({
+        pins: pins,
+        shot: this.state.shot + 1,
+        frames: newFrames,
+        frame: newFrame
+      });
+    }
+  }
+
+  handleReset() {
+    const { currFrame } = this.state;
+    this.setState({
+      shot: 1,
+      currFrame: currFrame + 1,
+      pins: Array(10).fill(0)
+    });
+  }
+  
   render() {
     return (
       <div>
