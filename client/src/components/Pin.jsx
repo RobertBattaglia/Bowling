7dc5fddc29a58e2482f8373777bdb47b37b35965
Robert Battaglia <r083rt164@gmail.com>
2019-07-03 14:42:32 -0400

    scoreboard and pin styling

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -4,11 +4,11 @@ import { GiBowlingPin } from 'react-icons/gi';
 const Pin = ({ state }) => {
   return state ? (
     <React.Fragment>
-      <GiBowlingPin size={48} />
+      <GiBowlingPin size={68} />
     </React.Fragment>
   ) : (
     <React.Fragment>
-      <GiBowlingPin size={48} color="red" />
+      <GiBowlingPin size={68} color={'firebrick'} style={{ opacity: '.75' }} />
     </React.Fragment>
   );
 };
